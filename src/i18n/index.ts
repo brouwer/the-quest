d18218ed193b4811dec7d1368cdabc040fd5bde6
brouwer <brouwer@users.noreply.github.com>
2023-02-23 18:39:13 +0100

    adding languages, and allow missing props

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,12 +2,21 @@ import { createI18n } from "vue-i18n"
 
 import en from "./en.json"
 import nb from "./nb.json"
+import de from "./de.json"
+import nl from "./nl.json"
+import fr from "./fr.json"
+import fi from "./fi.json"
+import it from "./it.json"
+import pt from "./pt.json"
+import tr from "./tr.json"
+import es from "./es.json"
+import ro from "./ro.json"
 
 type MessageSchema = typeof en
 
-const messages = { en, nb }
+const messages = { en, nb, de, nl, fr, fi, it, pt, tr, es, ro }
 
-export const i18n = createI18n<[MessageSchema], "en" | "nb">({
+export const i18n = createI18n<[MessageSchema], "en">({
   locale:
     window.localStorage.getItem("app-language") ||
     navigator.language?.split("-")[0] ||
